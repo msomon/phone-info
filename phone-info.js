35fb7fbf5e8813a1a22be2dd58f7038b7e6743ef
msomon <hsomon940@gmail.com>
2022-03-01 12:29:16 +0600

    sdd phone detail

--- phone-info.js
+++ phone-info.js
@@ -4,7 +4,8 @@ const addingInput=()=>{
   searchInput.value=''
 
   const url = `
-  https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  https://openapi.programming-hero.com/api/phones?search=${searchText}
+  `
   fetch(url)
   .then(res=>res.json())
   .then(data =>loadData(data.data))
@@ -14,6 +15,9 @@ const loadData =(phones)=>{
   // console.log(phones);
   const showingData = document.getElementById('search-phone')
   showingData.textContent=''
+  if(phones.length==0){
+    return alert('no result found')
+  }
   for(const phone of phones){
     // console.log(phone.slug)
     const div = document.createElement('div');
@@ -37,18 +41,21 @@ const collectData = phoneId =>{
 
 const displayData =(detail)=>{
   console.log(detail);
-const phoneDetail = document.getElementById('phone-detail')
+const phoneDetail = document.getElementById('phone-detail');
+phoneDetail.textContent=''
 const div = document.createElement('div')
+div.classList.add('phone-detail')
 div.innerHTML=`
-<img src="${detail.image}" alt="">
-<h4>brand:${detail.brand}</h4>
+<img class="image" src="${detail.image}" alt="">
+<h4>Brand:${detail.brand}</h4>
 <h4>Phone name:${detail.name}</h4>
 <h4>Release date:${detail.releaseDate}</h4>
-<h4>storage:${detail.mainFeatures.storage}</h4>
+<h4>Storage:${detail.mainFeatures.storage}</h4>
 <h4>Displaysize:${detail.mainFeatures.displaySize}</h4>
 <h4>Bluetooth:${detail.others.Bluetooth}</h4>
 <h4>USB:${detail.others.USB}</h4>
 <h4>Gps:${detail.others.GPS}</h4>
+<h4>Gps:${detail.others.WLAN}</h4>
 `
 phoneDetail.appendChild(div)
 }
